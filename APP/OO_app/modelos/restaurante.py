b4dac72d0d3a7dd0cccbebd4c68d2f516db5d1ea
Felipe Tomé or 2lMaKeR <120343427+2lMaKeR@users.noreply.github.com>
2024-05-07 16:47:59 -0300

    redefinindo patterns da classe

--- APP/OO_app/modelos/restaurante.py
+++ APP/OO_app/modelos/restaurante.py
@@ -1,17 +1,29 @@
 class Restaurante: # - class sempre com letra maiúscula como Restaurante
-    def __initi__(self,nome,categoria):
-        self.nome=''
-        self.categoria=''
-        self.ativo=False
+    restaurantes=[]
+
+    def __init__(self,nome,categoria):
+        self._nome=nome.title()
+        self._categoria=categoria.upper()
+        self._ativo=False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome}|{restaurante.categoria}|{restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome do restaurante'.ljust(20)}|{'Categoria'.ljust(20)}|{'Status'}")
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante.nome.ljust(20)}|{restaurante.categoria.ljust(20)}|{restaurante.ativo}')
 
-restaurante_plaza=Restaurante('Plaza','Gourmet')
-restaurante_pizza=Restaurante('Pizza Suprema','Italiana')
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativado'
+    
+    def alternar_estado(self):
+        self._ativo=not self._ativo
+
+restaurante_plaza=Restaurante('plaza','Gourmet')
+restaurante_pizza=Restaurante('pizza suprema','Italiana')
 
 Restaurante.listar_restaurantes()
